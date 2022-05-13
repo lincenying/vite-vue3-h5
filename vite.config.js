4f808a07a1421f13ed396533b581a952974cc279
林岑影 <lincenying@qq.com>
2022-05-13 15:01:19 +0800

    commit on 2022-05-13 15:01:19

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 const path = require('path')
 
 import { loadEnv } from 'vite'
-import styleImport from 'vite-plugin-style-import'
+import { createStyleImportPlugin, AndDesignVueResolve, VantResolve, ElementPlusResolve, NutuiResolve, AntdResolve } from 'vite-plugin-style-import'
 import vue from '@vitejs/plugin-vue'
 import vueSvgPlugin from 'vite-plugin-vue-svg'
 import WindiCSS from 'vite-plugin-windicss'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
@@ -19,9 +20,20 @@ export default ({ mode }) => {
             }
         },
         plugins: [
+            createHtmlPlugin({
+                inject: {
+                    data: {
+                        VITE_APP_ENV: process.env.VITE_APP_ENV,
+                        VITE_APP_API_DOMAIN: process.env.VITE_APP_API_DOMAIN,
+                        VITE_APP_API: process.env.VITE_APP_API,
+                        MODE: mode
+                    }
+                }
+            }),
             vue(),
             vueSvgPlugin(),
-            styleImport({
+            createStyleImportPlugin({
+                resolves: [AndDesignVueResolve(), VantResolve(), ElementPlusResolve(), NutuiResolve(), AntdResolve()],
                 libs: [
                     {
                         libraryName: 'ant-design-vue',
@@ -42,6 +54,15 @@ export default ({ mode }) => {
                         esModule: true,
                         resolveStyle: name => {
                             return `vant/es/${name}/style/index`
+                        }
+                    },
+                    {
+                        libraryName: 'element-ui',
+                        resolveStyle: name => {
+                            return `element-ui/lib/theme-chalk/${name}.css`
+                        },
+                        resolveComponent: name => {
+                            return `element-ui/lib/${name}`
                         }
                     },
                     {
