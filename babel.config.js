a6575b0896bcca93dfe9e2c600a3b5d65cfec48b
林岑影 <lincenying@qq.com>
2022-09-09 22:51:40 +0800

    commit on 2022-09-09 22:51:40

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,1 @@
-module.exports = {
-    presets: ['@vue/cli-plugin-babel/preset'],
-    plugins: [
-        [
-            'import',
-            {
-                libraryName: 'vant',
-                libraryDirectory: 'es',
-                style: true
-            },
-            'vant'
-        ],
-        '@vue/babel-plugin-jsx'
-    ]
-}
+module.exports = {}
