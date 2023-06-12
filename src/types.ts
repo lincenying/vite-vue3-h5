ccb434db1347bf01a4ef3ac3e93e1f1334275a1d
林岑影 <lincenying@qq.com>
2023-06-12 16:42:44 +0800

    chore: typescript

--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,5 @@
 import type { AxiosRequestConfig } from 'axios'
 
-export interface anyObject {
-    [propName: string]: any
-}
-
-export interface anyArray {
-    [index: number]: any
-}
-
 export type Fn = (...args: any[]) => void
 
 export interface GlobalStore {
@@ -28,11 +20,11 @@ export interface Article {
 }
 
 export interface ApiReturn {
-    get(url: string, params: Record<string, any>, headers?: Record<string, any>): Promise<any>
-    post(url: string, data: Record<string, any>, headers?: Record<string, any>): Promise<any>
-    put(url: string, data: Record<string, any>, headers?: Record<string, any>): Promise<any>
-    delete(url: string, data: Record<string, any>, headers?: Record<string, any>): Promise<any>
-    file(url: string, data: Record<string, any>, headers?: Record<string, any>): Promise<any>
-    RESTful(url: string, params: Record<string, any>, method: 'get' | 'post' | 'put' | 'delete', headers?: Record<string, any>): Promise<any>
-    fetch(url: string, params: Record<string, any>, payload: AxiosRequestConfig, cancelToken?: any): Promise<any>
+    get(url: string, params: Obj, headers?: Obj): Promise<any>
+    post(url: string, data: Obj, headers?: Obj): Promise<any>
+    put(url: string, data: Obj, headers?: Obj): Promise<any>
+    delete(url: string, data: Obj, headers?: Obj): Promise<any>
+    file(url: string, data: Obj, headers?: Obj): Promise<any>
+    RESTful(url: string, params: Obj, method: 'get' | 'post' | 'put' | 'delete', headers?: Obj): Promise<any>
+    fetch(url: string, params: Obj, payload: AxiosRequestConfig, cancelToken?: any): Promise<any>
 }
