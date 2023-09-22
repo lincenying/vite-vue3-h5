0039fe51b42fc601d682bd1a8f787a922eb40876
林岑影 <lincenying@qq.com>
2023-09-22 15:27:07 +0800

    chore: update eslint.config

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import { readFile } from 'node:fs/promises'
+import eslintConfig from '@lincy/eslint-config'
+import plugin from '@unocss/eslint-plugin'
+
+const autoImport = JSON.parse(
+    await readFile(new URL('./.eslintrc-auto-import.json', import.meta.url)),
+)
+
+const config = eslintConfig(
+    undefined,
+    {
+        plugins: {
+            '@unocss': plugin,
+        },
+        rules: plugin.configs.recommended.rules,
+    },
+    {
+        languageOptions: {
+            globals: {
+                ...autoImport.globals,
+            },
+        },
+    },
+)
+
+function a() {}
+
+export default config
